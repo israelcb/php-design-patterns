c74892ac2bb3d07a78b7742c6dad1a97328a3b3b
rozcovo <rozcovo@cpan.org>
2020-03-08 19:42:46 -0300

    Capítulo 1 - PHP and PDO

---
+++ 1_php_and_POO/MobileSniffer/Client.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+include_once("MobileSniffer.php");
+
+class Client {
+    private $mobSniff;
+
+    function __construct() {
+        $this->mobSniff = new MobileSniffer();
+        echo "Device = " . $this->mobSniff->findDevice() . "<br/>";
+        echo "Browser = " . $this->mobSniff->findBrowser() . "<br/>";
+    }
+}
+$trigger = new Client();
+?>
